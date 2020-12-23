3f69b8f1691bbb500f97fa8e4aaf91514ddb332d
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-12-23 19:57:40 +0100

    added username display in home view

--- src/main/java/ui/controller/HeaderBarController.java
+++ src/main/java/ui/controller/HeaderBarController.java
@@ -2,16 +2,22 @@ package ui.controller;
 
 import core.facade.UserFacade;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import main.SplitPay;
 import ui.path.UserNavigationPath;
 import ui.path.StoreOwnerNavigationPath;
+import javafx.scene.control.Label;
 
+import javax.jws.soap.SOAPBinding;
 import java.io.IOException;
 
 public class HeaderBarController {
+
+    @FXML
+    private Label nickName;
 
     /**
      * This method redirects to the myTransactionsView
@@ -21,6 +27,16 @@ public class HeaderBarController {
     public void goToHomeView(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource(UserNavigationPath.homeView));
         SplitPay.window.setScene(new Scene(root));
+    }
 
+    @FXML
+    private void initialize() {
+        String display;
+        if(UserFacade.getUserFacade().isStoreOwner()) {
+            display = "Store owner : ";
+        } else {
+            display = "User : ";
+        }
+        nickName.setText(display + UserFacade.getUserFacade().getLoggedUser().getNickname());
     }
 }
