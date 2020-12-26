d6710ff9a32e2353b8d8275921854150ff8b3a4f
AyoubMoujane <moujane.ayoub@hotmail.com>
2020-12-26 11:01:06 +0100

    Added first tests

---
+++ src/test/java/splitParticipationTests/AdvancedSplitTests.java
@@ -0,0 +1,4 @@
+package splitParticipationTests;
+
+public class AdvancedSplitTests {
+}
---
+++ src/test/java/splitParticipationTests/BasicSplitTests.java
@@ -0,0 +1,65 @@
+package splitParticipationTests;
+
+import server.facade.SplitServerFacade;
+import server.exception.SplitNotFoundException;
+import server.models.Participant;
+import server.models.Split;
+import org.junit.Before;
+import org.junit.Test;
+import server.utils.SplitUtilities;
+
+import java.lang.reflect.Field;
+
+public class BasicSplitTests {
+    @Before
+    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field instance = SplitServerFacade.class.getDeclaredField("instance");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
+    @Test
+    public void getSplitByCode() throws Exception {
+        SplitServerFacade facade = SplitServerFacade.getInstance();
+        String splitCode = facade.createSplit(1,"owner0",27.3,"new year bowling","freesplit");
+        Split expectedSplit = new Split(splitCode,1,"owner0",27.3,"new year bowling","freesplit");
+        System.out.println(expectedSplit);
+        System.out.println(facade.getSplitByCode(splitCode));
+
+        if(!facade.getSplitByCode(splitCode).equals(expectedSplit)){throw new Exception("Didn't get the expected split");}
+    }
+
+    @Test(expected = SplitNotFoundException.class)
+    public void getSplitByWrongCode() throws Exception {
+        SplitServerFacade facade = SplitServerFacade.getInstance();
+        String splitCode=SplitUtilities.generateSplitCode();
+        facade.createSplit(1,"owner0",27.3,"new year bowling","freesplit");
+        facade.getSplitByCode(splitCode);
+    }
+
+    @Test
+    public void getParticipantById() throws Exception {
+        SplitServerFacade facade = SplitServerFacade.getInstance();
+        String splitCode = facade.createSplit(1,"owner0",27.3,"new year bowling","freesplit");
+        Participant expectedParticipant = new Participant(1,"owner0");
+        Participant returnedParticipant = facade.getSplitByCode(splitCode).getParticipantById(expectedParticipant.getId());
+        System.out.println(expectedParticipant);
+        System.out.println(returnedParticipant);
+
+        if(!expectedParticipant.equals(returnedParticipant)){
+            throw new Exception("Didn't get the expected participant");
+        }
+    }
+
+    @Test
+    public void getParticipantByWrongId() throws Exception {
+        SplitServerFacade facade = SplitServerFacade.getInstance();
+        String splitCode = facade.createSplit(1,"owner0",27.3,"new year bowling","freesplit");
+        Participant expectedParticipant = null;
+        Participant returnedParticipant = facade.getSplitByCode(splitCode).getParticipantById(123456);
+
+        if(returnedParticipant!=expectedParticipant){
+            throw new Exception("Didn't get the expected participant");
+        }
+    }
+}
