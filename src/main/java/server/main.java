cebc944f8b48416da36381b8b18e61733e660fff
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-01-06 00:08:22 +0100

    Testing and instantiating a split with a real StoreOwner

--- src/main/java/server/main.java
+++ src/main/java/server/main.java
@@ -1,5 +1,6 @@
 package server;
 
+import core.models.StoreOwner;
 import server.facade.SplitServerFacade;
 import server.models.split.Item;
 
@@ -13,12 +14,33 @@ public class main {
     {
         SplitServerFacade facade = SplitServerFacade.getInstance();
         facade.createFreeSplit(1,"testUser",63.2,"bowling",null);
+
+        /**
+         * Creating an item list for an itemSplit
+         */
         Item pizza = new Item("margarita",4.00);
         Item drink = new Item("coke",1.50);
         Item desert = new Item("tiramisu",4.50);
         Item[] items = new Item[]{pizza, drink, desert};
-        System.out.println("Free split : " + facade.createFreeSplit(2,"User 2",32.2,"Pizza",null));
-        System.out.println("Item split : " + facade.createItemSplit(1,"testUser","Restaurant",items,null));
+
+        /**
+         * Instantiate a store owner present in the database as the receiver of the created splits
+         */
+        StoreOwner storeOwner = new StoreOwner("1",
+                "delarte34@hotmail.com",
+                "0767342312",
+                "362 521 879 00034",
+                "delarte34",
+                "delarte34080",
+                Float.parseFloat("743.67"),
+                "null",
+                "Del arte restaurant",
+                "145 Rue Alphonse Beau de Rochas, 34170 Castelnau");
+
+        System.out.println("Free split : " + facade.createFreeSplit(2,"User 2",32.2,"Pizza",storeOwner));
+        System.out.println("Item split : " + facade.createItemSplit(1,"testUser","Restaurant",items,storeOwner));
+
+
 
 
         try
