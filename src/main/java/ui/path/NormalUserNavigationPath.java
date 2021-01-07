ae8ec1463c0b5b89d544af457378d63a33c19ce4
Julien Wiegandt <wiegandtjulien2@gmail.com>
2021-01-07 22:13:47 +0100

    manageCreditCard ui boosted

--- src/main/java/ui/path/NormalUserNavigationPath.java
+++ src/main/java/ui/path/NormalUserNavigationPath.java
@@ -4,8 +4,9 @@ public class NormalUserNavigationPath {
     public static String chooseFriendView = "../../view/authPath/sendMoneyToFriend/chooseFriendView.fxml";
     public static String chooseAmountView = "../../view/authPath/sendMoneyToFriend/chooseAmountView.fxml";
 
-    public static String creditCardView = "../../view/authPath/CreditCardView.fxml";
-    public static String addCreditCardView = "../../view/authPath/AddCreditCardView.fxml";
+    public static String creditCardView = "../../view/authPath/manageCreditCard/CreditCardView.fxml";
+    public static String addCreditCardView = "../../view/authPath/manageCreditCard/AddCreditCardView.fxml";
+    public static String editCreditCardView = "../../view/authPath/manageCreditCard/editCreditCardView.fxml";
 
     public static String groupView = "../../view/authPath/GroupView.fxml";
     public static String addGroupView = "../../view/authPath/AddGroupView.fxml";
@@ -13,5 +14,4 @@ public class NormalUserNavigationPath {
     public static String editGroupView = "../../view/authPath/EditGroupView.fxml";
 
 
-    public static String editCreditCardView = "../../view/authPath/editCreditCardView.fxml";
 }
