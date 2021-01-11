0aa91dec4f8b2d6f6658a6c69942d8ed359efc02
Julien Wiegandt <wiegandtjulien2@gmail.com>
2021-01-11 14:34:51 +0100

    nickname regex enable - and _ now

--- src/main/java/util/RegexPattern.java
+++ src/main/java/util/RegexPattern.java
@@ -6,17 +6,17 @@ public class RegexPattern {
     public static Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
     public static Pattern phonePattern = Pattern.compile("^[0-9]{10}+$");
     public static Pattern textPattern = Pattern.compile("^[a-zA-Z]+$");
-    public static Pattern passwordPattern = Pattern.compile("^[a-zA-Z0-9 @#$%^&-+=():;.,?/!§%*µ£#{}]+$");
-    public static Pattern nicknamePattern = Pattern.compile("^[a-zA-Z0-9 @#$%^&-+=():;.,?/!§%*µ£#{}]+$");
+    public static Pattern passwordPattern = Pattern.compile("^[a-zA-Z0-9 @#$%^&\\-_+=():;.,?/!§%*µ£#{}]+$");
+    public static Pattern nicknamePattern = Pattern.compile("^[a-zA-Z0-9 @#$%^&\\-_+=():;.,?/!§%*µ£#{}]+$");
     public static Pattern siretPattern = Pattern.compile("^[a-zA-Z0-9]{14}$");
     public static Pattern decimalPattern = Pattern.compile("^[0-9]{1,9}([,.][0-9]{1,2})?$");
 
     public static Pattern numberPattern = Pattern.compile("\\d{16}");
-    public static Pattern namePattern = Pattern.compile("^[A-Za-z ]*$");
+    public static Pattern namePattern = Pattern.compile("^[A-Za-z ]+$");
     public static Pattern datePattern = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
     public static Pattern cvvPattern = Pattern.compile("\\d{3}");
 
     public static Pattern ibanPattern = Pattern.compile("^[a-zA-Z0-9]+$");
     public static Pattern bicPattern = Pattern.compile("^[a-zA-Z0-9]+$");
-    public static Pattern labelPattern =Pattern.compile("^[a-zA-Z0-9 @#$%^&-+=():;.,?/!§%*µ£#{}]+$");
+    public static Pattern labelPattern =Pattern.compile("^[a-zA-Z0-9 @#$%^&\\-_+=():;.,?/!§%*µ£#{}]+$");
 }
