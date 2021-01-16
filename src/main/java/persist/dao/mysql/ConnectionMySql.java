3cc62614072634fb436f8f509fdabf2ab1985fd1
AyoubMoujane <moujane.ayoub@hotmail.com>
2021-01-16 15:24:26 +0100

    Using string builder for connection url and config for credentials

--- src/main/java/persist/dao/mysql/ConnectionMySql.java
+++ src/main/java/persist/dao/mysql/ConnectionMySql.java
@@ -1,8 +1,12 @@
 package persist.dao.mysql;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionMySql {
 
@@ -10,6 +14,8 @@ public class ConnectionMySql {
      *
      */
     public static Connection connection;
+
+    private String MYSQL_DB_HOST,MYSQL_DB_NAME,MYSQL_DB_PORT,MYSQL_DB_USERNAME,MYSQL_DB_PW;
 
     public ConnectionMySql(){
         getConnection();
@@ -27,9 +33,32 @@ public class ConnectionMySql {
             System.out.println("MySQL JDBC Driver Registered!");
             connection = null;
 
+            try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");) {
+
+                Properties prop = new Properties();
+
+                // load a properties file
+                prop.load(input);
+
+                // get the property value and print it out
+                MYSQL_DB_HOST = prop.getProperty("MYSQL_DB_HOST");
+                MYSQL_DB_NAME = prop.getProperty("MYSQL_DB_NAME");
+                MYSQL_DB_PORT = prop.getProperty("MYSQL_DB_PORT");
+                MYSQL_DB_USERNAME = prop.getProperty("MYSQL_DB_USERNAME");
+                MYSQL_DB_PW = prop.getProperty("MYSQL_DB_PW");
+
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
             try {
+                StringBuilder connectionURL = new StringBuilder();
+                connectionURL.append("jdbc:mysql://")
+                        .append(MYSQL_DB_HOST).append(":").append(MYSQL_DB_PORT).append("/")
+                        .append(MYSQL_DB_NAME);
                 connection = DriverManager.
-                        getConnection("jdbc:mysql://" + "ec2-15-237-51-108.eu-west-3.compute.amazonaws.com" + ":" + "3306" + "/" + "splitpay_db", "useremote", "Polytech2021,");
+                        getConnection(connectionURL.toString(), MYSQL_DB_USERNAME, MYSQL_DB_PW);
             } catch (SQLException e) {
                 System.out.println("Connection Failed!:\n" + e.getMessage());
             }
